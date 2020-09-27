1be70cdedfc2cd98d9c4c0b7bb3fdf6a7c84711a
Christoph Buelter <buelter.christoph@gmail.com>
2020-09-27 12:52:26 +0200

    Setup initial project and models

---
+++ gamedoodle/core/models.py
@@ -0,0 +1,34 @@
+from datetime import date
+
+from django.db import models
+
+
+class TimestampedMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Event(TimestampedMixin, models.Model):
+    """A date on which to play certain games that can be voted for."""
+
+    name = models.CharField(max_length=256)
+    date = models.DateField(default=date.today)
+    games = models.ManyToManyField("SteamGame")
+
+
+class SteamGame(TimestampedMixin, models.Model):
+    """A Game on Steam."""
+
+    name = models.CharField(max_length=256)
+    appid = models.PositiveIntegerField()
+
+
+class Vote(TimestampedMixin, models.Model):
+    """A User votes to play a Game during a certain Event."""
+
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+    game = models.ForeignKey("SteamGame", on_delete=models.CASCADE)
+    username = models.CharField(max_length=256)
